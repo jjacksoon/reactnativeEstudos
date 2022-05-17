9ae61634ae5cbb02a9635abbb71c1a5a075a7c4e
Jackson Nascimento <jsnascimento1@hotmail.com>
2022-05-17 17:46:49 -0300

    Inicializa projeto e cria splashscreen

---
+++ quoteApp/src/global/styles/theme.ts
@@ -0,0 +1,12 @@
+export default{
+  colors:{
+    text:'#FFFFFF',
+    splashScreenText: '#34BCE5',
+    background: '#000049'
+  },
+  fonts:{
+    regular: 'Poppins_400Regular',
+    semibold: 'Poppins_600SemiBold',
+    bold: 'Poppins_700Bold',
+  }
+}
